ab689d486a16dfb6daac5c5bd71679a2540c84d1
MichaelNguyenTT <michaelnguyenn94@gmail.com>
2023-11-07 21:23:05 -0800

    Added comments
    Restructured code

--- app.js
+++ app.js
@@ -1,78 +1,64 @@
 'use strict'
 
+window.onload = init;
+
+function init() {
+    
+    const selectList = document.getElementById('dropdownList');
+    selectList.onchange = function () {
+        const displayInfo = document.getElementById('displayInfo');
+        displayInfo.innerHTML = '';
+    }
+    selectList.appendChild(new Option('Select A Team', ''))
+    loadTeamSelected(teams);
+    
+    const submitForm = document.getElementById('submitForm');
+    submitForm.onsubmit = function (event) {
+        event.preventDefault();
+        displayTeamInfo();
+    }
+    return false;
+}
+// stored data in an array of objects
 let teams = [
+    
     {code:"DAL", name:"Dallas Cowboys", plays:"Arlington, TX"},
     {code:"DEN", name:"Denver Broncos", plays:"Denver, CO"},
     {code:"HOU", name:"Houston Texans", plays:"Houston, TX"},
-    {code:"KAN", name:"Kansas City Chiefs", plays:"Kansas City, MO"},
-];   
+    {code:"KAN", name:"Kansas City Chiefs",
+    plays:"Kansas City, MO"},
+];
 
-// function to display dropdown items
-function loadteamSelected(_teams) {
+
+//When this function is called it will load dropdown menu
+function loadTeamSelected(_teams) {
     
-    const selectedTeam = document.getElementById('selectedList');
-    let teamsLength = _teams.length;
-
-    for (let i = 0; i < teamsLength; i++) {
-        // creates the option element and sets the text and value
-        let teamOption = new Option(teams[i].name, teams[i].code); 
-
-        console.log(selectedTeam.appendChild(teamOption));
-        selectedTeam.appendChild(teamOption);
-
-        // output of this function:
-        // name: "" and code: "" <- the stored value
-    }
-};
-
-function findTeam(_teamList, _code) {
-    let matchingList = [];
-
-    for (let i = 0; i < _teamList.length; i++) {
-        if (_teamList[i].code == _code) {
-            matchingList.push(_teamList[i])
-        }
-    }
-    return matchingList;
-};
-
-function getTeamName() {
-
-    const selectedTeam = document.getElementById('selectedList').value;
-    const displayInfo = document.getElementById('displayTeamInfo');
-    // passed the teams array and selectedteam value to the function
-    const teamList = findTeam(teams, selectedTeam); // returns the object in an array that contains selectedteam value
+    const selectList = document.getElementById('dropdownList');
     
-    // forEach loop to display each 
-    teamList.forEach((item) => { 
-        displayInfo.innerHTML = `You selected the ${item.name} (${item.code}) who plays in ${item.plays} ` 
+    _teams.forEach((item) => {
+        let teamOption = new Option(item.name, item.code);
+        selectList.appendChild(teamOption)
     })
 };
-
-window.onload = init; 
-    
-function init() {
-    //run the stateSelected()
-    loadteamSelected(teams);
-    const onSelectChange = document.getElementById('selectedList');
-    onSelectChange.onchange = function () {
-        const displayInfo = document.getElementById('displayTeamInfo');
-        displayInfo.innerHTML = ''
-    }
-
-    const submitForm = document.getElementById('selectForm');
-    submitForm.onsubmit = function (event) {
-        event.preventDefault();
-        getTeamName()
+// Call this function to find the team object == selectedlist value
+function findTeam(_teams, _code) {
+    for (let i = 0; i < _teams.length; i++) {
+        if (_teams[i].code == _code) {
+            return _teams[i]
+        } 
     }
 }
+// function to display chosen team name, code and plays
+function displayTeamInfo() {
 
+    const selectedTeam = document.getElementById('dropdownList').value;
+    const displayInfo = document.getElementById('displayInfo');
 
+    let teamInfo = findTeam(teams, selectedTeam); // returns the object in an array 
+    
+    displayInfo.innerHTML = `You selected the ${teamInfo.name} (${teamInfo.code}) who plays in ${teamInfo.plays} `
 
-
-
-
-
+}
 
 
 
